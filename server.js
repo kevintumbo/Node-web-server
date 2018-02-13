1063e31d2aa8b5fcdf6ec143f80936e3b59b2e28
Kevin Tumbo <kevin.tumbo39@gmail.com>
2018-02-13 21:09:57 +0300

    feat(basic-http-server): create a basic node server
    - setup express
    - create views and server
    - setup server
    
    [delivered]

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+const app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next)=>{
+  const now = new Date().toString();
+  const log = `${now}: ${req.method} ${req.originalUrl}`;
+
+  fs.appendFile('server.log', log + `\n` , (err) => {
+    if(err) {
+      console.log('unable to append to server.log.');
+    }
+  });
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintanance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome To this Express site'
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'An Error has occured'
+  })
+});
+
+app.listen(3000, () => {
+  console.log('Server is Up and Running');
+});
